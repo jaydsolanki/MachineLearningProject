fecb9b642ad59a704a413409072ad945eb243ccb
jaydsolanki <jayd_solanki@yahoo.com>
2016-11-26 17:17:05 -0500

    Added Base Django project

---
+++ MLProject/NewsGroups20/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Newsgroups20Config(AppConfig):
+    name = 'NewsGroups20'
---
+++ MLProject/NewsGroups20/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"title":"Home"}
+    return render(request, 'index.html', context)
