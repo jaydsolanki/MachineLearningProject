c3e40a8ac29e0da9c9619199effb03320ce73bd0
jaydsolanki <jayd_solanki@yahoo.com>
2016-11-26 20:32:13 -0500

    Added UI for Naive Byes

--- MLProject/NewsGroups20/views.py
+++ MLProject/NewsGroups20/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import algos.multinomialnb as naive_bayes
+from algos.getdata import total_categories
 
 
 def index(request):
     context = {"title":"Home"}
     return render(request, 'index.html', context)
+
+
+def classification(request):
+    if request.method=="GET":
+        context = {"title":"Classification"}
+        return render(request, "classification.html", context)
+    else:
+        classifier = request.POST.get('classifier')
+        response = {}
+        if classifier=="naive_bayes":
+            smoothing_factor = int(request.POST.get('smoothing_factor','2'))
+            approach = request.POST.get('approach')
+            n_gram = request.POST.get('n_gram', '1,3').split(",")
+            n_gram_range = (int(n_gram[0]), int(n_gram[1]))
+            # print("Smoothing: "+str(smoothing_factor)+"; approach: "+str(approach)+"; n_gram: "+str(n_gram_range))
+            if approach=="tf_idf":
+                predicted, cnf_matrix, recall, precision, accuracy, error_rate = naive_bayes.make_model_tf_idf(n_gram_range, smoothing_factor)
+                #redicted, cnf_matrix, recall, precision, accuracy, error_rate = make_model_tf_idf((1,3), 2)
+            else:
+                predicted, cnf_matrix, recall, precision, accuracy, error_rate = naive_bayes.make_model(n_gram_range, smoothing_factor)
+            # predicted, cnf_matrix, recall, precision, accuracy, error_rate = (1,2,3,4,5,6)
+            response = {"recall": recall, "precision": precision, "accuracy": accuracy, "error_rate": error_rate, "confusion_matrix":cnf_matrix.tolist()
+                        ,"labels": total_categories}
+        else:
+            pass
+        return HttpResponse(json.dumps(response), content_type="application/json", status=200)
--- MLProject/algos/multinomialnb.py
+++ MLProject/algos/multinomialnb.py
@@ -4,15 +4,15 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-# from sklearn.metrics import zero_one_score # NOT FOUND
 from sklearn.metrics import precision_score
+# from sklearn.metrics import zero_one_score # NOT FOUND
 from .getdata import *
 
 
-def train(train_data, ngram_range=(1,1)):
+def train(train_data, ngram_range=(1,1), alpha=2):
     count_vect = CountVectorizer(analyzer='word', ngram_range=ngram_range,stop_words='english')
     X_train_counts = count_vect.fit_transform(train_data.data)
-    clf = MultinomialNB(alpha=2).fit(X_train_counts, train_data.target)
+    clf = MultinomialNB(alpha=alpha).fit(X_train_counts, train_data.target)
     return clf, count_vect
 
 
@@ -21,18 +21,18 @@ def test(test_data, clf, count_vect):
     predicted = clf.predict(X_new_counts)
     cnf_matrix = confusion_matrix(test_data.target, predicted)
     recall = recall_score(test_data.target, predicted, average='weighted')
-    precision_score = precision_score(test_data.target, predicted, average='weighted')
+    precision = precision_score(test_data.target, predicted, average='weighted')
     accuracy = accuracy_score(test_data.target, predicted, normalize=True)
     error_rate = 1 - accuracy
-    return predicted, cnf_matrix, recall, precision_score, accuracy, error_rate
+    return predicted, cnf_matrix, recall, precision, accuracy, error_rate
 
 
-def train_with_tf_idf(train_data, ngram_range=(1,1)):
+def train_with_tf_idf(train_data, ngram_range=(1,1), alpha=2):
     count_vect = CountVectorizer(analyzer='word', ngram_range=ngram_range,stop_words='english')
     X_train_counts = count_vect.fit_transform(train_data.data)
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-    clf = MultinomialNB(alpha=2).fit(X_train_tfidf, train_data.target)
+    clf = MultinomialNB(alpha=alpha).fit(X_train_tfidf, train_data.target)
     return clf, tfidf_transformer, count_vect
 
 
@@ -40,26 +40,26 @@ def test_tf_idf(test_data, clf, tfidf_transformer, count_vect):
     X_new_counts = count_vect.transform(test_data.data)
     X_new_tf = tfidf_transformer.transform(X_new_counts)
     predicted = clf.predict(X_new_tf)
-    #clf.score(X_new_tf,test_data.target)
     cnf_matrix = confusion_matrix(test_data.target, predicted)
     recall = recall_score(test_data.target, predicted, average='weighted')
-    precision_score = precision_score(test_data.target, predicted, average='weighted')
+    precision = precision_score(test_data.target, predicted, average='weighted')
     accuracy = accuracy_score(test_data.target, predicted, normalize=True)
     error_rate = 1-accuracy
+    return predicted,cnf_matrix,recall,precision,accuracy,error_rate
 
-    return predicted,cnf_matrix,recall,precision_score,accuracy,error_rate
 
-
-def make_model():
+def make_model(ngram_range, alpha):
     train_data = get_train_data(random_state=42)
     test_data = get_test_data(random_state=42)
-    clf, count_vect = train(train_data, ngram_range=(1,5))
-    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test(test_data, clf, count_vect)
+    clf, count_vect = train(train_data, ngram_range=ngram_range, alpha=alpha)
+    predicted, cnf_matrix, recall, precision, accuracy, error_rate=test(test_data, clf, count_vect)
+    return predicted, cnf_matrix, recall, precision, accuracy, error_rate
 
 
-def make_model_tf_idf():
+def make_model_tf_idf(ngram_range, alpha):
     train_data = get_train_data(random_state=42)
     test_data = get_test_data(random_state=42)
-    clf, tfidf_transformer, count_vect = train_with_tf_idf(train_data, ngram_range=(1,5))
-    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test_tf_idf(test_data, clf,
+    clf, tfidf_transformer, count_vect = train_with_tf_idf(train_data, ngram_range=ngram_range, alpha=alpha)
+    predicted, cnf_matrix, recall, precision, accuracy, error_rate=test_tf_idf(test_data, clf,
                                                                                      tfidf_transformer, count_vect)
+    return predicted, cnf_matrix, recall, precision, accuracy, error_rate
