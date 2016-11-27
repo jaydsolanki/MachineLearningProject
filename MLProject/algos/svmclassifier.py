c5669a4099c56d0422e5743166d5d1169aa1797c
ganapathyaiyer <ganapathy@edu.chat>
2016-11-26 19:42:32 -0500

    SVM

--- MLProject/algos/svmclassifier.py
+++ MLProject/algos/svmclassifier.py
@@ -1,46 +1,58 @@
-# import numpy as np
-# X=['send us your password','send us your review','review your password','review us' ,'send your password','send us your account']
-# y = ['spam', 'ham', 'ham', 'spam', 'spam', 'spam']
-# from sklearn.naive_bayes import MultinomialNB
-# clf = MultinomialNB()
-# clf.fit(np.array(X).reshape(-1,1), y)
-#
-# print(clf.predict('your review'))
-
-from sklearn.datasets import fetch_20newsgroups
-categories = ['alt.atheism', 'soc.religion.christian',
-              'comp.graphics', 'sci.med',
-'comp.os.ms-windows.misc',
-'comp.sys.ibm.pc.hardware',
-'comp.sys.mac.hardware',
-'comp.windows.x','misc.forsale','rec.autos',
-'rec.motorcycles',
-'rec.sport.baseball',
-'rec.sport.hockey','talk.politics.misc',
-'talk.politics.guns',
-'talk.politics.mideast','sci.crypt',
-'sci.electronics',
-'sci.space','talk.religion.misc']
-twenty_train = fetch_20newsgroups(subset='train',
-    categories=categories, shuffle=True, random_state=42)
+from .getdata import *
 from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer(analyzer='word', ngram_range=(1, 3),stop_words='english')
-X_train_counts = count_vect.fit_transform(twenty_train.data)
-#X_train_counts.shape
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+# from sklearn.metrics import zero_one_score # NOT FOUND
+from sklearn.metrics import precision_score
 from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-#X_train_tf.shape
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import SGDClassifier
-clf = SGDClassifier(loss="hinge",  alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, twenty_train.target)
 
-docs_new = ['New drug to cure hairfall', 'OpenGL on the GPU is fast','Microsoft releases anniversary updates for the latest version of windows'
-            ,'Apple introduces new touchpad for macbook','Tsunami hits Japan']
-X_new_counts = count_vect.transform(docs_new)
-X_new_tf = tfidf_transformer.transform(X_new_counts)
+def train(train_data, ngram_range=(1,1)):
+    count_vect = CountVectorizer(analyzer='word', ngram_range=ngram_range, stop_words='english')
+    X_train_counts = count_vect.fit_transform(train_data.data)
+    clf = SGDClassifier(loss="hinge", alpha=1e-3, n_iter=5, random_state=42).fit(X_train_counts, train_data.target)
+    return clf,count_vect
 
-predicted = clf.predict(X_new_tf)
+def test(test_data, clf, count_vect):
+    X_new_counts = count_vect.transform(test_data.data)
+    predicted = clf.predict(X_new_counts)
+    cnf_matrix = confusion_matrix(test_data.target, predicted)
+    recall = recall_score(test_data.target, predicted, average='weighted')
+    precision_score = precision_score(test_data.target, predicted, average='weighted')
+    accuracy = accuracy_score(test_data.target, predicted, normalize=True)
+    error_rate = 1 - accuracy
+    return predicted, cnf_matrix, recall, precision_score, accuracy, error_rate
 
-for doc, category in zip(docs_new, predicted):
-    print('%r => %s' % (doc, twenty_train.target_names[category]))
+def train_with_tf_idf(train_data, ngram_range=(1,1)):
+    count_vect = CountVectorizer(analyzer='word', ngram_range=ngram_range, stop_words='english')
+    X_train_counts = count_vect.fit_transform(train_data.data)
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    clf = SGDClassifier(loss="hinge", alpha=1e-3, n_iter=5, random_state=42).fit(X_train_tfidf, train_data.target)
+    return clf,count_vect
+
+def test_tf_idf(test_data, clf,tfidf_transformer, count_vect):
+    X_new_counts = count_vect.transform(test_data.data)
+    X_new_tf = tfidf_transformer.transform(X_new_counts)
+    predicted = clf.predict(X_new_counts)
+    cnf_matrix = confusion_matrix(test_data.target, predicted)
+    recall = recall_score(test_data.target, predicted, average='weighted')
+    precision_score = precision_score(test_data.target, predicted, average='weighted')
+    accuracy = accuracy_score(test_data.target, predicted, normalize=True)
+    error_rate = 1 - accuracy
+    return predicted, cnf_matrix, recall, precision_score, accuracy, error_rate
+
+def make_model():
+    train_data = get_train_data(random_state=42)
+    test_data = get_test_data(random_state=42)
+    clf, count_vect = train(train_data, ngram_range=(1,5))
+    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test(test_data, clf, count_vect)
+
+
+def make_model_tf_idf():
+    train_data = get_train_data(random_state=42)
+    test_data = get_test_data(random_state=42)
+    clf, tfidf_transformer, count_vect = train_with_tf_idf(train_data, ngram_range=(1,5))
+    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test_tf_idf(test_data, clf,
+                                                                                     tfidf_transformer, count_vect)
