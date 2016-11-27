0651781403e304687fb5e1719718ce7daf8ed8a1
ganapathyaiyer <ganapathy@edu.chat>
2016-11-26 19:19:35 -0500

    Removed Unnecessary code

--- MLProject/algos/multinomialnb.py
+++ MLProject/algos/multinomialnb.py
@@ -6,7 +6,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 # from sklearn.metrics import zero_one_score # NOT FOUND
 from sklearn.metrics import precision_score
-
 from .getdata import *
 
 
@@ -20,15 +19,12 @@ def train(train_data, ngram_range=(1,1)):
 def test(test_data, clf, count_vect):
     X_new_counts = count_vect.transform(test_data.data)
     predicted = clf.predict(X_new_counts)
-    predict_list = predicted.tolist()
-    for j in range(len(predict_list)):
-        predict_dict={}
-        for i in range(0, len(clf.classes_)):
-            predict_dict[clf.classes_[i]]=predict_list[j][i]
-        sorted_predict_dict = sorted(predict_dict.items(), key=lambda x: x[1], reverse=True)
-        print (str(test_data.data[j])+" : "+str(total_categories[sorted_predict_dict[0][0]]))
-        print ("\n\n\n\n")
-    return sorted_predict_dict
+    cnf_matrix = confusion_matrix(test_data.target, predicted)
+    recall = recall_score(test_data.target, predicted, average='weighted')
+    precision_score = precision_score(test_data.target, predicted, average='weighted')
+    accuracy = accuracy_score(test_data.target, predicted, normalize=True)
+    error_rate = 1 - accuracy
+    return predicted, cnf_matrix, recall, precision_score, accuracy, error_rate
 
 
 def train_with_tf_idf(train_data, ngram_range=(1,1)):
@@ -40,35 +36,30 @@ def train_with_tf_idf(train_data, ngram_range=(1,1)):
     return clf, tfidf_transformer, count_vect
 
 
-def test_tf_idf(train_data, test_data, clf, tfidf_transformer, count_vect):
+def test_tf_idf(test_data, clf, tfidf_transformer, count_vect):
     X_new_counts = count_vect.transform(test_data.data)
     X_new_tf = tfidf_transformer.transform(X_new_counts)
     predicted = clf.predict(X_new_tf)
-    predict_list = predicted.tolist()
+    #clf.score(X_new_tf,test_data.target)
     cnf_matrix = confusion_matrix(test_data.target, predicted)
     recall = recall_score(test_data.target, predicted, average='weighted')
     precision_score = precision_score(test_data.target, predicted, average='weighted')
     accuracy = accuracy_score(test_data.target, predicted, normalize=True)
     error_rate = 1-accuracy
-    for j in range(len(predict_list)):
-        predict_dict={}
-        for i in range(0, len(clf.classes_)):
-            predict_dict[clf.classes_[i]]=predict_list[j][i]
-        sorted_predict_dict = sorted(predict_dict.items(), key=lambda x: x[1], reverse=True)
-        print (str(test_data.data[j])+" : "+str(total_categories[sorted_predict_dict[0][0]]))
-        print ("\n\n\n\n")
-    return sorted_predict_dict
+
+    return predicted,cnf_matrix,recall,precision_score,accuracy,error_rate
 
 
 def make_model():
     train_data = get_train_data(random_state=42)
     test_data = get_test_data(random_state=42)
     clf, count_vect = train(train_data, ngram_range=(1,5))
-    test(train_data, test_data, clf, count_vect)
+    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test(train_data, test_data, clf, count_vect)
 
 
 def make_model_tf_idf():
     train_data = get_train_data(random_state=42)
     test_data = get_test_data(random_state=42)
     clf, tfidf_transformer, count_vect = train_with_tf_idf(train_data, ngram_range=(1,5))
-    test_tf_idf(train_data, test_data, clf, tfidf_transformer, count_vect)
+    predicted, cnf_matrix, recall, precision_score, accuracy, error_rate=test_tf_idf(train_data, test_data, clf,
+                                                                                     tfidf_transformer, count_vect)
